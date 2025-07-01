2fdd3eebdf5092f667eb7e5532bde83b6f5af949
lokeshvijay7 <lokeshlokesh93661@gmail.com>
2025-07-01 12:44:55 +0530

    Best Time to Buy and Sell Stocks (Leetcode 121)

---
+++ Array/python/best_time_to_buy_and_sell_stocks.py
@@ -0,0 +1,10 @@
+def maxProfit(self, prices):
+        min_price = float("inf")
+        max_profit = 0
+
+        for i in prices:
+            if i < min_price:
+                min_price = i 
+            elif  i- min_price > max_profit:
+                max_profit = i - min_price
+        return max_profit
