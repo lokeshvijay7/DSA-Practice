b997d4770f18a7abe3dc8f87e23729efff656b21
lokeshvijay7 <lokeshlokesh93661@gmail.com>
2025-08-19 13:06:32 +0530

    right triangle

---
+++ pattern/right-triangle.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+for i in range(1, num + 1):
+  for j in range(i):
+    print("*", end=" ")
+  print()
+  
+# *         
+# * *       
+# * * *     
+# * * * *   
+# * * * * * 
--- pattern/square-pattern.py
+++ pattern/square-pattern.py
@@ -1,3 +1,4 @@
+# https://www.geeksforgeeks.org/problems/square-pattern/1?utm_medium=collab_striver_ytdescription&utm_campaign=pattern_1&utm_source=youtube
 num = int(input("Enter a number: "))
 for i in range(num):
     for j in range(num):
