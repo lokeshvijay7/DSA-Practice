9e5bb80c5fc008494b8ac9822e9f9c493aab178f
lokeshvijay7 <lokeshlokesh93661@gmail.com>
2025-08-20 10:48:37 +0530

    Inverted star pyramid

---
+++ pattern/inverted-star-pyramid.py
@@ -0,0 +1,18 @@
+num = int(input("Enter a number:"))
+for i in range(num):
+    for j in range(i+1):
+        print(" ", end=" ")
+    for j in range(i, num-1):
+        print("*", end=" ")
+    for j in range(i, num):
+        print("*", end=" ")
+    
+        
+    print()    
+
+
+  # * * * * * * * * *    
+  #   * * * * * * *      
+  #     * * * * *        
+  #       * * * 
+  #         * 
